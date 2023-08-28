1ea4ff2bed336d6779d8b6203c893de7fce128f9
AeybelV <aeybelvarghese@gmail.com>
2023-08-27 23:38:25 -0500

    Added basic email patterns

--- src/patterns.h
+++ src/patterns.h
@@ -5,15 +5,29 @@
 
 // Hyperscan RegEx Patterns
 const char *patterns[] = {
-    "\\b[a-f0-9]{32}\\b", // MD5
-    "\\b[a-f0-9]{64}\\b", // SHA256
+    "\\b[a-f0-9]{32}\\b",             // MD5
+    "\\b[a-f0-9]{64}\\b",             // SHA256
+    "\\b.+@gmail\.com\\b",            // Gmail
+    "\\b.+@yahoo\.com\\b",            // Yahoo
+    "\\b.+@outlook\.com\\b",          // Outlook
+    "\\b.+@proton\.me\\b",            // Proton Mail
+    "\\b.+@aol\.com\\b",              // AOL Mail
+    "\\b.+@icloud\.com\\b",           // iCloud Mail
+    "\\b.+@hotmail\.com\\b",          // Hotmail Mail
     0
 };
 
 // Used for output, the index in this array matches up with index of patterns
 const char *pattern_labels[]={
     "MD5",
-    "SHA256"
+    "SHA256",
+    "Gmail Email Address",
+    "Yahoo Email Address",
+    "Outlook Email Address",
+    "Proton Mail Email Address",
+    "AOL Email Address",
+    "iCloud Email Address",
+    "Hotmail Email Address"
 };
 
 // At compile time, pattern ID's are created for all the hash patterns. The ID corresponds to its respective hash in hash_names and patterns
